b03c6756b71daa0b9b97c7e0abd2f361402a3b73
fakebmarchete <fakebmarchete@gmail.com>
2020-03-09 15:32:04 -0300

    fazendo commit

--- executandoRegex/UsandoJs.js
+++ executandoRegex/UsandoJs.js
@@ -6,10 +6,14 @@ console.log('Métodos da RegExp...')
 //no texto que está sendo passado no metodo teste
 console.log(regexNove.test(texto))
 console.log(regexNove.exec(texto))
-
+//vai pegar todas as letras entre a e f
 const regexLetras = /[a-f]/g
-//console.log('Métodos da string...')
-//console.log(texto.match(regexLetras))
-//console.log(texto.search(regexLetras))
-//console.log(texto.replace(regexLetras, 'Achei'))
-//console.log(texto.split(regexLetras))
+console.log('Métodos da string...')
+//vai encontrar as letras a,b,c,e,f
+console.log(texto.match(regexLetras))
+//Vai pegar o primeiro elemento e todos os outros do indice 20 ( primeiro elemento ) até o final
+console.log(texto.search(regexLetras))
+//vai substituir o elemento que encontrou na regex pelo elemento de algum outro texto, a,b,c,d,e vai ser substituído 
+console.log(texto.replace(regexLetras, 'Achei'))
+//vai dar um split de tudo que bateu com a regex
+console.log(texto.split(regexLetras))
