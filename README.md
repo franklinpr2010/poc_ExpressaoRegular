55db9a0c1d6cbd648ad30faa163eb6b7fdae900e
fakebmarchete <fakebmarchete@gmail.com>
2020-03-09 16:35:07 -0300

    comentários em python

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 
-# curso-regex
 
-## -Instalar o VSC
-## -Instalar o Plugin Code Runner ( Crtl + Alt +  N)
+
+# Expressões Regulares   
+
+## -Instalar o VSC   
+## -Instalar o Plugin Code Runner ( Crtl + Alt +  N)   
 
 
 ## Site para testar as Expressões  
 https://regex101.com/  
 
-# Expressões Regulares
+## Metacaracteres
+
+Ponto (.) = Um caractere qualquer  
+[] - Conjunto de caracteres.  
+[^] - Conjunto negado de caracteres  
+? - Opcional  
+* - Zero ou mais  
++ - Mais, um ou mais.  
+{n, m} - De n até m.  
+^ - Inicío da linha.
+$ - Fim da linha.
+\b - Inicio ou fim da palavra 
+
--- caracteres/CaractereSimples.js
+++ caracteres/CaractereSimples.js
@@ -1,11 +1,16 @@
 const texto = '1,2,3,4,5,6,a.b c!d?e'
 
 const regexVirgula = /,/
+//Vai quebrar o texto tirando as virgulas e para cada vez que ele encontra vírgula gera um novo elemento.
 console.log(texto.split(regexVirgula))
+//vai achar a vírgula no índice 1, 
 console.log(texto.match(regexVirgula))
-
+//Vai achar 6 virgulas, colocou a flag global achou todas
 console.log(texto.match(/,/g))
+//Vai achar a letra A, mas é case sensitive
 console.log(texto.match(/A/g))
+//Vai achar a letra A
 console.log(texto.match(/A/gi))
 console.log(texto.match(/2/g))
+//vai colocar b c!d, se tiver esse texto ele vai encontrar
 console.log(texto.match(/b c!d/))
