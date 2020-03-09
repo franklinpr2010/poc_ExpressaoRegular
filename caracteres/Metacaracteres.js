99a7f4d2dbca148b3015b86145b19535074b1795
fakebmarchete <fakebmarchete@gmail.com>
2020-03-09 16:52:22 -0300

    metacaracte

--- caracteres/Metacaracteres.js
+++ caracteres/Metacaracteres.js
@@ -1,7 +1,11 @@
 // . ? * + - ^ $ | [ ] { } ( ) \ :
 const texto = '1,2,3,4,5,6,a.b c!d?e'
+//Usando o ponto como o valor literal com ESCAPE ('\')
 const regexPonto = /\./g
 console.log(texto.split(regexPonto))
 
+//Colocando a virgula que não é um metacaractere, Ou ( \| ), Ou ( \. ) Ou ( \? ) Ou (!) Ou / (Espaço)
+// g - Flag Global vai achar todas as referencias desse texto
 const regexSimbolos = /,|\.|\?|!| /g
+//Quebrou todos os que tem o metacteres
 console.log(texto.split(regexSimbolos))
