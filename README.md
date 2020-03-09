6f46a9e8d7680136fe4f7b3a21392f6a95cf443b
fakebmarchete <fakebmarchete@gmail.com>
2020-03-09 11:40:13 -0300

    primeiro exemplo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # curso-regex
+
+## -Instalar o VSC
+## -Instalar o Plugin Code Runner ( Crtl + Alt +  N)
+
+
+## Site para testar as Expressões  
+https://regex101.com/  
+
--- executandoRegex/Flags.js
+++ executandoRegex/Flags.js
@@ -2,6 +2,10 @@
 // i - ignore case
 
 const texto = 'Carlos assinou o abaixo-assinado.'
+//Vai procurar letra C maiúsculo ou letra ab
 console.log(texto.match(/C|ab/))
+//Vai procurar letra c minúsculo ou letra ab - Flag i (Fora da expressão regular) - i (case sensitive)
 console.log(texto.match(/c|ab/i))
+//Vai procurar letra ab minúsculo ou letra c - Flag i (Fora da expressão regular) - i (case sensitive) - g (Global - vai procurar
+//até achar todas as ocorrências)
 console.log(texto.match(/ab|c/gi))
