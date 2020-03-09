10dc44e89ab46acad6610aa4d314c6e5ab27489f
fakebmarchete <fakebmarchete@gmail.com>
2020-03-09 15:41:26 -0300

    comentários em python

--- executandoRegex/UsandoPython.py
+++ executandoRegex/UsandoPython.py
@@ -4,7 +4,11 @@ texto = '0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f'
 
 pattern9 = re.compile('9')
 match1 = re.search(pattern9, texto)
+# start() - indice de início onde achou um determinado elemento.
+# end() - indice final de determinado elemento
+# group(0) - vai pegar o elemento que foi encontrado na busca
 print "Posicoes: %s, %s; Valor: %s." % (match1.start(), match1.end(), match1.group(0))
 
+# vai pegar todas as ocorrências de uma expressão dentro de um texto.
 valores = re.findall('[a-f]', texto)
 print "Valores: %s" % valores
