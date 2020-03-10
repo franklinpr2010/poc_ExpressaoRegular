3fef227770c06d55767cbedaa3dd10220667598b
fakebmarchete <fakebmarchete@gmail.com>
2020-03-10 12:08:50 -0300

    intervalos

--- conjuntos/Conjuntos.js
+++ conjuntos/Conjuntos.js
@@ -1,9 +1,11 @@
 const texto = '1,2,3,4,5,6,a.b c!d?e[f'
 
 // para definir uma classe (ou conjunto) de caracteres usa []
+//números pares 2,4,6,8 ele vai achar
 const regexPares = /[02468]/g
 console.log(texto.match(regexPares))
 
 const texto2 = 'João não vai passear na moto.'
+//n com o a sem o til e com o til
 const regexComESemAcento = /n[aã]/g
 console.log(texto2.match(regexComESemAcento))
--- conjuntos/Intervalos.js
+++ conjuntos/Intervalos.js
@@ -1,6 +1,9 @@
 const texto = '1,2,3,4,5,6,a.b c!d?e[f'
-
+//pegar o conjunto das letras de 'a' a 'z' com todas as letras e vai fazer uma match nessas letras
 console.log(texto.match(/[a-z]/g))
+//pegar as letras de 'b' a 'd' com todas as letras e vai fazer uma match nessas letras
 console.log(texto.match(/[b-d]/g))
+// vai pegar o intervalo entre 2 e 4.
 console.log(texto.match(/[2-4]/g))
+//A até Z (Maíusculo) e de 1 até 3
 console.log(texto.match(/[A-Z1-3]/gi))
