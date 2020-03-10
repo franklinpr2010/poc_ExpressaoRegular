cc4f502256a6e93f0e787cab7766d4fa22046696
fakebmarchete <fakebmarchete@gmail.com>
2020-03-10 11:43:07 -0300

    problemas com ponto

--- caracteres/ProblemaPonto.js
+++ caracteres/ProblemaPonto.js
@@ -1,5 +1,12 @@
+
+//O '\n' não é resolvido no curinga
 const texto = 'Bom\ndia'
+//Se fosse o tab o ponto aceitaria
+const texto2 = 'Bom\tdia'
+//três caracteres qualquer
 console.log(texto.match(/.../gi))
+console.log(texto2.match(/.../gi))
+//não existe quatro caracteres em sequencia, porque o '\n' não é resolvido pelo curinga
 console.log(texto.match(/..../gi)) // o ponto não engloba o \n
 
 // dotall - algums linguagens tem um flag /exp/s, mas JS não!
