6f085e249810dcbdf5094c19a4adf7c5b3801597
fakebmarchete <fakebmarchete@gmail.com>
2020-03-10 15:21:14 -0300

    conjunto com metacaracteres

--- conjuntos/ConjuntosComMetacaracteres.js
+++ conjuntos/ConjuntosComMetacaracteres.js
@@ -1,9 +1,14 @@
 const texto = '.$+*?-'
 
+//Dentro de um contexto de um conjunto não precisa colocar o escape para dizer que é literal
 console.log(texto.match(/[+.?*$]./g)) // não precisa de escape dentro do conjunto
+
 console.log(texto.match(/[$-?]/g)) // isso é um intervalo (range)
 
-// NÃO é um intervalo (range)...
+//demtro de um conjunto ele se comporta como um literal e não como qualquer caractere
+console.log(texto.match(/[.]/g)) // isso é um intervalo (range)
+
+// NÃO é um intervalo porque colocou um ESCAPE antes
 console.log(texto.match(/[$\-?]/g))
 console.log(texto.match(/[-?]/g))
 
