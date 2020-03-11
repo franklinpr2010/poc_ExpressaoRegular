de5a2c30c0c33cefff5bdd8c484dbec6039a7149
fakebmarchete <fakebmarchete@gmail.com>
2020-03-11 11:34:55 -0300

    conjuntos negados'

--- conjuntos/ConjuntosNegados.js
+++ conjuntos/ConjuntosNegados.js
@@ -1,9 +1,13 @@
 const texto = '1,2,3,a.b c!d?e[f'
 
+//pega exatamente aquilo que não é dígito, tudo que não é 0-9 ele pega
 console.log(texto.match(/\D/g))
+//negação de 0 ate 9, tudo que não é zero e 9
 console.log(texto.match(/[^0-9]/g))
+//tudo que não é digito, exclamação, interrogação, virgula, ponto, nem espaço em branco
 console.log(texto.match(/[^\d!\?\[\s,\.]/g))
 
 const texto2 = '1: !"#$%&\'()*+,-./ 2: :;<=>?@'
 
+//existem dois intervalos ai, do exclamação até o ':' e do dois ponto até o '@'
 console.log(texto2.match(/[^!-/:-@\s]/g))
--- conjuntos/Shorthands.js
+++ conjuntos/Shorthands.js
@@ -1,13 +1,17 @@
 const texto = `1,2,3,4,5,6,a.b c!d?e\r	-
 f_g`
 
+//vai pegar dentro do texto todos os dígitos
 console.log(texto.match(/\d/g)) // números [0-9]
+//vai obter todos os não dígitos.
 console.log(texto.match(/\D/g)) // não números [^0-9]
-
+//corresponde a letras, números e underline
 console.log(texto.match(/\w/g)) // caracteres [a-zA-Z0-9_]
+//Tudo aquilo que não é letras, números e underline
 console.log(texto.match(/\W/g)) // não caracteres [^a-zA-Z0-9_]
-
+//Espaços em branco, o tab entra e o caractere de '\n' - nova linha
 console.log(texto.match(/\s/g)) // espaço [ \t\n\r\f]
+//Tudo o que não é espaço trouxe no console
 console.log(texto.match(/\S/g)) // não espaço [^ \t\n\r\f]
 
 // \b \B
