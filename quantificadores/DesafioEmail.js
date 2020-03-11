857e855d14d2fca0f6f7e974d7b021199e335c1f
fakebmarchete <fakebmarchete@gmail.com>
2020-03-11 18:27:15 -0300

    Desafios de quantificadores

--- quantificadores/DesafioEmail.js
+++ quantificadores/DesafioEmail.js
@@ -6,11 +6,15 @@ Os e-mails dos convidados são:
     - maria_silva@registro.br
     - rafa.sampaio@yahoo.com
 `
-
+//vai pegar de 'a' minúsculo a 'z' minúsculo de [A-Z] e '0' a '9', '+' um ou mais, precedido de '@'
 console.log(texto.match(/[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.[a-zA-Z0-9_]{2,4}/g))
+// '\w' shorthand de 'a' a 'z' e '0' a '9', logo após um '.' literal e após um shorthand de 2 a 4
 console.log(texto.match(/\w+@\w+\.\w{2,4}/g))
+//vai colocar um grupo que vai ter um shorthands de caracteres mais o ponto, vai colocar o '+' para ter 
+//vários caracteres agrupados, após mais o @, após vai ter um ponto literal com scape '/.' e para finalizar
+//Colocar um Shorthand de '\z{2,4}'
 console.log(texto.match(/[\w.]+@\w+\.\w{2,4}/g))
+//maria_silva@registro.br '\w{2,4}\.?' opcional e no final '\w{0,2}' representando o 'br'
 console.log(texto.match(/[\w.]+@\w+\.\w{2,4}\.?\w{0,2}/g))
 
-// no futuro...
 console.log(texto.match(/[\w.]+@\w+\.\w{2,4}(\.\w{2})?/g))
