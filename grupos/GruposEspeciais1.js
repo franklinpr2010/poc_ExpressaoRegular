d8baf3a7cf8e0ecc74424fa5ef9d61737432a756
fakebmarchete <fakebmarchete@gmail.com>
2020-03-12 17:42:03 -0300

    Grupos Aninhados

--- grupos/GruposEspeciais1.js
+++ grupos/GruposEspeciais1.js
@@ -1,12 +1,17 @@
+//GRUPOS QUE OLHAM PARA FRENTE E NÃO PARA TRÁS.
+
 const texto = 'João é calmo, mas no transito fica nervoso.'
 
+//Vai pegar o texto com caracteres especiais, o '+', ele vai acabar quebrando
 console.log(texto.match(/[\wÀ-ú]+/gi))
 
-// Positive lookahead
+// (?=,) - quer que procure a frente da palavra, procure uma virgula na frente de uma palavra
 console.log(texto.match(/[\wÀ-ú]+(?=,)/gi))
+// (?=\.) - a única palavra que tem um ponto na frente é a palavra nervoso
 console.log(texto.match(/[\wÀ-ú]+(?=\.)/gi))
+//?= - tudo para trás vai achar o anterior antes do ', mas' ---- Resultado: [ 'calmo' ]
 console.log(texto.match(/[\wÀ-ú]+(?=, mas)/gi))
 
-// Negative lookahead
+
 console.log(texto.match(/[\wÀ-ú]+\b(?!,)/gi))
 console.log(texto.match(/[\wÀ-ú]+[\s|\.](?!,)/gi))
--- grupos/GruposEspeciais2.rb
+++ grupos/GruposEspeciais2.rb
@@ -1,9 +1,12 @@
+#GRUPOS QUE OLHAM PARA FRENTE E NÃO PARA TRÁS.
+
 texto = 'supermercado superação hiperMERCADO Mercado'
 
+#Vai pegar as palavras que começam com super, não é opcional depois tem letras com acento com uma ou mais ocorrências.
 puts texto.scan(/(?:super)[\wÀ-ú]+/i).join(' ')
 
-# Positive Lookbehind
+#qualquer palavra que tenha um prefixo da palavra super
 puts texto.scan(/(?<=super)[\wÀ-ú]+/i).join(' ')
 
-# Negative Lookbehind
+#quer dar match em mercado de algo que não está precedido da palavra super
 puts texto.scan(/(?<!super)mercado/i)
