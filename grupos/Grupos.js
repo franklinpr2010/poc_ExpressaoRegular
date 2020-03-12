1a06dfca4b6fb7b281b36d5f55b0a69122889e18
fakebmarchete <fakebmarchete@gmail.com>
2020-03-12 11:56:33 -0300

    retrovisor

--- grupos/Grupos.js
+++ grupos/Grupos.js
@@ -1,5 +1,8 @@
 const texto1 = 'O José Simão é muito engraçado... hehehehehehe'
+//agrupando um quantificador de um ou mais
 console.log(texto1.match(/(he)+/g))
-
 const texto2 = 'http://www.site.info www.escola.ninja.br google.com.ag'
+//criou um grupo o // precisa ter um escape, o www vem seguido de um ponto literal, então coloca um escape 
+// o (www\.)? torna um grupo opcional, depois abriu um quantificador .\w{2,} de dois a qualquer coisa
+//no final (\.\w{2})? um grupo de 2 caracteres 
 console.log(texto2.match(/(http:\/\/)?(www\.)?\w+\.\w{2,}(\.\w{2})?/g))
--- grupos/Retrovisor.js
+++ grupos/Retrovisor.js
@@ -1,9 +1,17 @@
 const texto1 = '<b>Destaque</b><strong>Forte</strong><div>Conteudo</div>'
+//Quer capturar as tags de abertura, vai criar um grupo para capturar a parte interna da TAG
+//dentro do grupo vai ter um shorthand '<(\w+)>', '.*', qualquer coisa que tiver dentro da TAG
+//'<\/\1>' o valor que foi capturado no primeiro grupo vai ter que ser capturado aqui '\1'
 console.log(texto1.match(/<(\w+)>.*<\/\1>/g))
 
 const texto2 = 'Lentamente é mente muito lenta.'
+//os dois grupos são obrigatórios, então ele colocou a palavra (lenta)(mente) dentro de um grupo
+//'\2' está pegando o conteúdo do grupo 2 que é mente e após usando o '.*' para obter o restante do texto
+//E após aplicando conteúdo do primeiro grupo '\1' e depois o ponto literal '\.'
 console.log(texto2.match(/(lenta)(mente).*\2.*\1\./gi))
-console.log(texto2.match(/(?:lenta)(mente).*\1/gi)) // ?: não guarda
+
+//
+console.log(texto2.match(/(?:lenta)(mente).*\1/gi)) 
 
 console.log(texto2.match(/(lenta)(mente)/gi))
 console.log(texto2.match(/(lenta)(mente)?/gi))
